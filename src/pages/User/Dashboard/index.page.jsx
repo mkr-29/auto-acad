47a46a2ff12731b32ca09faedde1f6fa8e7cfba5
Mayank Kumar <mayank.kumar@daakia.co.in>
2024-10-04 14:25:35 +0530

    Dashboard Started

--- src/pages/User/Dashboard/index.page.jsx
+++ src/pages/User/Dashboard/index.page.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Navbar from '../Components/Navbar/index.page'
-import {ReactComponent as AutoAcadLogo} from '../../../assets/Logo/AutoAcadLogo.svg'
-import './Dashboard.scss'
+import React from "react";
+import Navbar from "../Components/Navbar/index.page";
+import { ReactComponent as AutoAcadLogo } from "../../../assets/Logo/AutoAcadLogo.svg";
+import "./Dashboard.scss";
 
 export default function Dashboard() {
   return (
-    <div className='dashboard'>
-      <Navbar/>
-      <div className='dashboard-main'>
-        <AutoAcadLogo className='dashboard-logo'/>
+    <div className="dashboard">
+      <Navbar />
+      <div className="dashboard-main">
+        <button className="dashboard-menu">
+          <i className="fas fa-bars"></i>
+          <span>MENU</span>
+        </button>
+        <AutoAcadLogo className="dashboard-logo" />
       </div>
     </div>
-  )
+  );
 }
