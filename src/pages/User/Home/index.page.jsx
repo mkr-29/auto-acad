976e946c2ee6a2e8a50b86ed77ac780bf35c48d6
Mayank Kumar <mayank.k9802@gmail.com>
2025-03-29 10:48:08 +0530

    UPDATES:: ABOUT SECTION ROUTE UPDATED

--- src/pages/User/Home/index.page.jsx
+++ src/pages/User/Home/index.page.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { ReactComponent as AutoAcadLogo } from "../../../assets/Logo/AutoAcadLogo.svg";
 import "./Home.scss";
 import Navbar from "../../../Components/Navbar/index.page";
-import About from "./Components/About/index.page";
 import Footer from "../../../Components/Footer/index.page";
+import About from "../components/About/index.page";
 
 export default function Home() {
   return (
